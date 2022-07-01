3e4da1ccafe61b3d5698a3eea20922c82b777781
Hugo Dzin <humodz065@gmail.com>
2022-07-01 09:24:11 -0300

    selectShopsByCategory

--- src/containers/CategoriesScreen/index.js
+++ src/containers/CategoriesScreen/index.js
@@ -1,31 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { selectShopsByCategory } from '../../store/shopsSlice';
 import { setSelectedShop } from '../../store/searchShopsSlice';
-import { useMemo } from 'react';
-import { sorted } from '../../utils';
 import { Category } from '../../components/Category';
-
-const collator = new Intl.Collator(['pt-BR', 'pt'], {
-  usage: 'sort',
-  sensitivity: 'base',
-  ignorePunctuation: true,
-  numeric: true,
-});
 
 export function CategoriesScreen() {
   const dispatch = useDispatch();
 
   const shopsByCategory = useSelector(selectShopsByCategory);
-
-  const sortedCategories = useMemo(() => (
-    Array
-      .from(shopsByCategory)
-      .sort(([name1], [name2]) => collator.compare(name1, name2))
-      .map(([name, shops]) => [
-        name,
-        sorted(shops, (shop1, shop2) => collator.compare(shop1.nome, shop2.nome))
-      ])
-  ), [shopsByCategory]);
 
   const onClickShop = shop => {
     window.scrollTo(0, 0);
@@ -35,11 +16,11 @@ export function CategoriesScreen() {
   return (
     <div>
       {
-        sortedCategories
-          .map(([name, shops], i) => (
+        shopsByCategory
+          .map(([category, shops], i) => (
             <Category
               key={i}
-              name={name}
+              name={category}
               shops={shops}
               onClickShop={onClickShop}
             ></Category>
--- src/store/shopsSlice.js
+++ src/store/shopsSlice.js
@@ -1,5 +1,12 @@
 import { createAsyncThunk, createSelector, createSlice } from '@reduxjs/toolkit';
-import { groupBy } from '../utils';
+import { groupBy, sorted } from '../utils';
+
+const collator = new Intl.Collator(['pt-BR', 'pt'], {
+  usage: 'sort',
+  sensitivity: 'base',
+  ignorePunctuation: true,
+  numeric: true,
+});
 
 const initialState = {
   shops: [],
@@ -8,7 +15,15 @@ const initialState = {
 export const selectShops = state => state.shops.shops;
 
 export const selectShopsByCategory = createSelector([selectShops], (shops) => {
-  return groupBy(shops, shop => shop.itensSeguimento[0].seguimento);
+  const shopsByCategory = groupBy(shops, shop => shop.itensSeguimento[0].seguimento);
+
+  return Array
+    .from(shopsByCategory)
+    .sort(([category1], [category2]) => collator.compare(category1, category2))
+    .map(([category, shops]) => [
+      category,
+      sorted(shops, (shop1, shop2) => collator.compare(shop1.nome, shop2.nome))
+    ]);
 });
 
 export const fetchShops = createAsyncThunk(
